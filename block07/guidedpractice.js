4a370669153ef05317839c812ca153f32edb4a54
Djana Ramos <mrs.djana.ramos@gmail.com>
2025-02-05 17:25:01 -0600

    block work

---
+++ block07/guidedpractice.js
@@ -0,0 +1,45 @@
+//Number 1 of guided practice is describing how to brush your teeth in 8 or more steps
+//Step 1 turn on water
+//Step 2 Wet your tooth brush
+//Step 3 Add tooth paste to tooth brush
+//Step 4 Put toothbrush with tooth paste in mouth
+//Step 5 Begin to move toothbrush back and forward over top teeth going from back to front
+//Step 6 Begin to move toothbrush back and forward over bottom teeth going from back to front
+//Step 7 Spit toothpaste out and gargle with water
+//Step 8 Repeat steps 1 through 7 for the next 5 minutes
+
+//Question 2
+let numberOfCows=50;
+
+//Question 3
+console.log("Hello World!");
+
+//Question 4
+const greeting= "Hello ";
+const place= "Disneyland";
+console.log(greeting + place + "!");
+
+//Queston 5
+let numOfUmberlas= 10*3
+console.log(numOfUmberlas);
+
+//Question 6
+let num= 5 + 1
+console.log(num);
+
+//Question 7
+//Add an external script file to an html file. Already done on HTML guided practice
+
+//Question 8
+const greeting1= "Hello ";
+const place1= "Space ";
+const place2= "World!"
+console.log(greeting1 + place1 + place2);
+
+//Question 9 Concatenate strings using variables (1) Hello, (2) [space] (3) I am (4) [space] (5) Josh. Display over a dialog box
+let str1= "Hello,"
+let str2= "space ";
+let str3= "I am ";
+let str4= "space ";
+let str5= "Josh";
+alert(str1 + " " +  str2 + str3 + str4 + str5);
