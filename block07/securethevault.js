e1250c3896c2af4349083739cd184a2b7e968445
Djana Ramos <mrs.djana.ramos@gmail.com>
2025-02-05 19:26:24 -0600

    secure the vault assignment

---
+++ block07/securethevault.js
@@ -0,0 +1,27 @@
+//Create a html and js page
+//Link the pages together
+//Create a string for the user saying You have received this message because you have been chosen to open an important vault. Here is the secret combination: 121
+//Assign three variables
+//create three unique arithmetic operator
+//Add comments throughout the code to explain each step in the process
+//Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog
+
+alert(
+    "You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10-40-39"
+  );
+  //This code gives the user an alert when coming to the page. It is a statement and ends with a semicolon
+  
+  let myVariable = 10*1;
+  console.log(myVariable);
+  //Expected output is 10 because 10 multiplied by 1 equals 10
+  
+  let num = 25 + 15
+  console.log(num);
+  //Expected output is 40
+  
+  let strg1= 5;
+  let strg2= 25;
+  let strg3= 9;
+  console.log(strg1 + strg2 + strg3)
+
+  //=== strictly equals one, making the if statement true and output 1
